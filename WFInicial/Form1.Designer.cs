0cd60a9d835e5c9b7421f7f1575b5fcaece47028
ANDREZA PEREIRA SANTOS <andrezap63@gmail.com>
2025-02-27 19:56:08 -0300

    Feat: Corrgido Form Ex1#

--- WFInicial/Form1.Designer.cs
+++ WFInicial/Form1.Designer.cs
@@ -28,54 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            button1 = new Button();
-            textBox1 = new TextBox();
+            lblNota1 = new Label();
+            btnCalcular = new Button();
+            txtNota1 = new TextBox();
+            txtNota2 = new TextBox();
+            lblNota2 = new Label();
             SuspendLayout();
             // 
-            // label1
+            // lblNota1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(91, 58);
-            label1.Name = "label1";
-            label1.Size = new Size(38, 15);
-            label1.TabIndex = 0;
-            label1.Text = "label1";
+            lblNota1.AutoSize = true;
+            lblNota1.Location = new Point(123, 102);
+            lblNota1.Name = "lblNota1";
+            lblNota1.Size = new Size(45, 15);
+            lblNota1.TabIndex = 0;
+            lblNota1.Text = "Nota 1:";
+            lblNota1.Click += label1_Click;
             // 
-            // button1
+            // btnCalcular
             // 
-            button1.Location = new Point(91, 141);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 1;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            btnCalcular.Location = new Point(207, 175);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(75, 23);
+            btnCalcular.TabIndex = 1;
+            btnCalcular.Text = "Calcular";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
             // 
-            // textBox1
+            // txtNota1
             // 
-            textBox1.Location = new Point(91, 99);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 2;
+            txtNota1.Location = new Point(182, 94);
+            txtNota1.Name = "txtNota1";
+            txtNota1.Size = new Size(100, 23);
+            txtNota1.TabIndex = 2;
+            // 
+            // txtNota2
+            // 
+            txtNota2.Location = new Point(182, 131);
+            txtNota2.Name = "txtNota2";
+            txtNota2.Size = new Size(100, 23);
+            txtNota2.TabIndex = 3;
+            // 
+            // lblNota2
+            // 
+            lblNota2.AutoSize = true;
+            lblNota2.Location = new Point(123, 139);
+            lblNota2.Name = "lblNota2";
+            lblNota2.Size = new Size(45, 15);
+            lblNota2.TabIndex = 4;
+            lblNota2.Text = "Nota 2:";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(textBox1);
-            Controls.Add(button1);
-            Controls.Add(label1);
+            ClientSize = new Size(433, 324);
+            Controls.Add(lblNota2);
+            Controls.Add(txtNota2);
+            Controls.Add(txtNota1);
+            Controls.Add(btnCalcular);
+            Controls.Add(lblNota1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Calcular Média das Notas";
+            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
-        private Button button1;
-        private TextBox textBox1;
+        private Label lblNota1;
+        private Button btnCalcular;
+        private TextBox txtNota1;
+        private TextBox txtNota2;
+        private Label lblNota2;
     }
 }
--- WFInicial/Form1.cs
+++ WFInicial/Form1.cs
@@ -6,5 +6,50 @@ namespace WFInicial
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double nota1 = double.Parse(txtNota1.Text);
+            double nota2 = double.Parse(txtNota2.Text);
+
+            double media = (nota1 + nota2) / 2;
+
+            if (media >= 7)
+            {
+                MessageBox.Show
+                    ("Ok", "Aprovado", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+            else if (media < 5)
+            {
+                MessageBox.Show
+                    ("Deu Ruim", "Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show
+                    ("Atenção", "Você está de recuperação", MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+            }
+            {
+                
+            }
+        }
     }
 }
