0aa524c3ce67fdb5f7a6e79060f8c4f4894e254b
andreza.psantos <andreza.psantos@L130627987W10-1>
2025-02-26 22:02:12 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleRevisaoAula/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleRevisaoAula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a 1 primeira Nota do Aluno!");
+            double nota = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a 2 primeira Nota do Aluno!");
+            double nota2 = Convert.ToDouble(Console.ReadLine());
+
+            double media = (nota + nota2) / 2;
+
+            if (media >= 7)
+            {
+                Console.WriteLine("Aprovado!");
+            }
+            else if (media < 5)
+            {
+                Console.WriteLine("Reprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Recuperação!");
+            }
+        }
+    }
+}
