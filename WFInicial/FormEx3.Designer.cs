4b43f6a4eca5ab651ed2b898b19ead8ec24b3df3
ANDREZA PEREIRA SANTOS <andrezap63@gmail.com>
2025-02-28 19:10:44 -0300

    feat: Ex3 corrigido no windows forms

--- WFInicial/FormEx3.Designer.cs
+++ WFInicial/FormEx3.Designer.cs
@@ -28,18 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            txtNum1 = new TextBox();
+            txtNum2 = new TextBox();
+            lblNumero1 = new Label();
+            lblNumero2 = new Label();
+            btnMaior = new Button();
             SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            txtNum1.Location = new Point(185, 78);
+            txtNum1.Name = "txtNum1";
+            txtNum1.Size = new Size(100, 23);
+            txtNum1.TabIndex = 0;
+            // 
+            // txtNum2
+            // 
+            txtNum2.Location = new Point(185, 119);
+            txtNum2.Name = "txtNum2";
+            txtNum2.Size = new Size(100, 23);
+            txtNum2.TabIndex = 1;
+            // 
+            // lblNumero1
+            // 
+            lblNumero1.AutoSize = true;
+            lblNumero1.Location = new Point(111, 79);
+            lblNumero1.Name = "lblNumero1";
+            lblNumero1.Size = new Size(63, 15);
+            lblNumero1.TabIndex = 2;
+            lblNumero1.Text = "Numero 1:";
+            // 
+            // lblNumero2
+            // 
+            lblNumero2.AutoSize = true;
+            lblNumero2.Location = new Point(111, 127);
+            lblNumero2.Name = "lblNumero2";
+            lblNumero2.Size = new Size(63, 15);
+            lblNumero2.TabIndex = 3;
+            lblNumero2.Text = "Numero 2:";
+            // 
+            // btnMaior
+            // 
+            btnMaior.Location = new Point(185, 169);
+            btnMaior.Name = "btnMaior";
+            btnMaior.Size = new Size(100, 25);
+            btnMaior.TabIndex = 4;
+            btnMaior.Text = "Mostrar Maior";
+            btnMaior.UseVisualStyleBackColor = true;
+            btnMaior.Click += btnMaior_Click;
             // 
             // FormEx3
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(643, 441);
+            Controls.Add(btnMaior);
+            Controls.Add(lblNumero2);
+            Controls.Add(lblNumero1);
+            Controls.Add(txtNum2);
+            Controls.Add(txtNum1);
             Name = "FormEx3";
             Text = "FormEx3";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtNum1;
+        private TextBox txtNum2;
+        private Label lblNumero1;
+        private Label lblNumero2;
+        private Button btnMaior;
     }
 }
