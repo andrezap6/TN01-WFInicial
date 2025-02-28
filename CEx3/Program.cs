2e4bcdac86b7a050f46d93a9fb128911127c2293
ANDREZA PEREIRA SANTOS <andrezap63@gmail.com>
2025-02-27 22:25:26 -0300

    feat: corrigo no Console

---
+++ CEx3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CEx3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o 1º número: ");
+            int num1 = int.Parse(Console.ReadLine()!);
+            Console.Write("\n Digite o 2º número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int maior = Math.Max(num1, num2);
+
+            Console.WriteLine($"\n O maior número é: {maior}");
+        }
+    }
+}
