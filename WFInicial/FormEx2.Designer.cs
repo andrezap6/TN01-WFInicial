7c008322a5bf22039934198e5434ee5e7632773e
ANDREZA PEREIRA SANTOS <andrezap63@gmail.com>
2025-02-27 22:13:07 -0300

    feat: Ex2 Corrigido

--- WFInicial/FormEx2.Designer.cs
+++ WFInicial/FormEx2.Designer.cs
@@ -28,12 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormEx2";
+            lblDiaSemana = new Label();
+            txtDiaSemana = new TextBox();
+            bntEscolher = new Button();
+            SuspendLayout();
+            // 
+            // lblDiaSemana
+            // 
+            lblDiaSemana.AutoSize = true;
+            lblDiaSemana.Location = new Point(129, 74);
+            lblDiaSemana.Name = "lblDiaSemana";
+            lblDiaSemana.Size = new Size(85, 15);
+            lblDiaSemana.TabIndex = 0;
+            lblDiaSemana.Text = "Dia da Semana";
+            lblDiaSemana.Click += label1_Click;
+            // 
+            // txtDiaSemana
+            // 
+            txtDiaSemana.Location = new Point(220, 66);
+            txtDiaSemana.Name = "txtDiaSemana";
+            txtDiaSemana.Size = new Size(100, 23);
+            txtDiaSemana.TabIndex = 1;
+            txtDiaSemana.TextChanged += txtDiaSemana_TextChanged;
+            // 
+            // bntEscolher
+            // 
+            bntEscolher.Location = new Point(245, 110);
+            bntEscolher.Name = "bntEscolher";
+            bntEscolher.Size = new Size(75, 23);
+            bntEscolher.TabIndex = 2;
+            bntEscolher.Text = "Escolher";
+            bntEscolher.UseVisualStyleBackColor = true;
+            bntEscolher.TextChanged += txtDiaSemana_TextChanged;
+            bntEscolher.Click += bntEscolher_Click;
+            // 
+            // FormEx2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(719, 503);
+            Controls.Add(bntEscolher);
+            Controls.Add(txtDiaSemana);
+            Controls.Add(lblDiaSemana);
+            Name = "FormEx2";
+            Text = "FormEx2";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblDiaSemana;
+        private TextBox txtDiaSemana;
+        private Button bntEscolher;
     }
 }
--- WFInicial/FormEx2.cs
+++ WFInicial/FormEx2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WFInicial
 {
@@ -15,6 +16,49 @@ namespace WFInicial
         public FormEx2()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDiaSemana_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bntEscolher_Click(object sender, EventArgs e)
+        {
+            byte opcao = byte.Parse(txtDiaSemana.Text);
+
+            switch (opcao)
+            {
+                case 1:
+                    MessageBox.Show("Domingo");
+                    break;
+                case 2:
+                    MessageBox.Show("Segunda-Feira");
+                    break;
+                case 3:
+                    MessageBox.Show("Terça-Feira");
+                    break;
+                case 4:
+                    MessageBox.Show("Quarta-Feira");
+                    break;
+                case 5:
+                    MessageBox.Show("Quinta-Feira");
+                    break;
+                case 6:
+                    MessageBox.Show("Sexta-Feira");
+                    break;
+                case 7:
+                    MessageBox.Show("Sábado");
+                    break;
+                default:
+                    MessageBox.Show("Dia inválido");
+                    break;
+            }
         }
     }
 }
