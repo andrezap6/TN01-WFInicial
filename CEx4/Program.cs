2c485f3af3a5cf1be7c50e52396a9e819d125898
ANDREZA PEREIRA SANTOS <andrezap63@gmail.com>
2025-02-28 19:20:02 -0300

    feat: Ex 4 corrigido no console

---
+++ CEx4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CEx4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o 1º número: ");
+            int num1 = int.Parse(Console.ReadLine()!);
+            Console.Write("\n Digite o 2º número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o 3º número: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int maior = Math.Max(num1, num2);
+            maior = Math.Max(maior, num3);
+
+            Console.WriteLine($"\n O maior número é: {maior}");
+        }
+    }
+}
