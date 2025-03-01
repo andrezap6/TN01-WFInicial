856bb669966177890049dd01fd2dcd08a669ea1c
ANDREZA PEREIRA SANTOS <andrezap63@gmail.com>
2025-02-28 22:22:39 -0300

    feat: Menu de exercicios

---
+++ CEx7/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CEx7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o valor do produto:");
+            string[] produtos = { "Arroz", "Feijão", "Macarrão", "Carne", "Frango", "Ovos" };
+            double[] precos = { 2.99, 3.99, 1.99, 15.99, 10.99, 5.99 };
+
+            for (int i = 0;  i < produtos.length;  1++)
+            {
+                Console.WriteLine("O produto mais barato é : + produtos[0] + " e custa: " + precos[0]);
+            }
+        
+        }
+    }
+}
